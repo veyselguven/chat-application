2655a99f6317aea40a5e6f20cf3919b709f03a35
veysel basbaydar <vgbasbaydar@gmail.com>
2022-02-22 14:57:15 -0800

    test

---
+++ public/chat.js
@@ -0,0 +1,32 @@
+//we connect with browser and server here
+const socket = io.connect("http://localhost:3000");
+
+const sender = document.getElementById("sender");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submitBtn");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
+
+submitBtn.addEventListener("click", () => {
+  socket.emit("chat", {
+    message: message.value,
+    sender: sender.value,
+  });
+});
+
+// we catch the information from socket here and we need to run
+
+socket.on("chat", (data) => {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.sender + ":</strong>" + data.message + "</p>";
+  message.value = "";
+});
+
+message.addEventListener("keypress", () => {
+  socket.emit("typing", sender.value);
+});
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p>" + data + "typing....</p>";
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+
+const port = 3000;
+const server = app.listen(port, () =>
+  console.log(`server is running at ${port}`)
+);
+
+//
+app.use(express.static("public"));
+
+// the things runs from localhost 3000
+const io = socket(server);
+
+// i check the connection here if any connection happens it knows here
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("chat", (data) => {
+    // we have to send all data information to browser , we do that one by emit, we can send all our connection(browser)
+    io.sockets.emit("chat", data);
+  });
+
+  //listening here
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
+});
